be1a0741d1db720882739f324459bd9d021cca6a
cowboymalboro1884 <nejkovdaniil@gmail.com>
2024-02-14 17:43:12 +0300

    feat(fragments): add Places, Feed and Profile fragments

---
+++ app/src/main/java/ru/hse/goodtrip/MainViewModel.java
@@ -0,0 +1,4 @@
+package ru.hse.goodtrip;
+
+public class MainViewModel {
+}
--- app/src/main/java/ru/hse/goodtrip/ui/places/PlacesViewModel.java
+++ app/src/main/java/ru/hse/goodtrip/ui/places/PlacesViewModel.java
@@ -1,16 +1,16 @@
-package ru.hse.goodtrip.ui.dashboard;
+package ru.hse.goodtrip.ui.places;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class PlacesViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+    public PlacesViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+        mText.setValue("There will be a places!");
     }
 
     public LiveData<String> getText() {
