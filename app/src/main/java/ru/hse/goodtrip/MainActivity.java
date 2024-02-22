1cd6909b920e109018e68ae74c2975bb94c04ddc
cowboymalboro1884 <nejkovdaniil@gmail.com>
2024-02-23 01:16:38 +0300

    style: erase extra imports

--- app/src/main/java/ru/hse/goodtrip/MainActivity.java
+++ app/src/main/java/ru/hse/goodtrip/MainActivity.java
@@ -1,29 +1,14 @@
 package ru.hse.goodtrip;
 
-import static android.app.PendingIntent.getActivity;
-
-import android.content.Intent;
-import android.credentials.CredentialManager;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import java.util.Objects;
-
-import com.google.android.libraries.identity.googleid.GetGoogleIdOption;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import ru.hse.goodtrip.databinding.ActivityMainBinding;
-import ru.hse.goodtrip.ui.login.LoginFragment;
 
 public class MainActivity extends AppCompatActivity {
 
