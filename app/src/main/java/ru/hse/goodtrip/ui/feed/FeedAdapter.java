56f55533ed4806f2f39261d5a621a94cb025b22d
cowboymalboro1884 <nejkovdaniil@gmail.com>
2024-02-23 18:23:55 +0300

    feat: add post layout

--- app/src/main/java/ru/hse/goodtrip/ui/feed/FeedAdapter.java
+++ app/src/main/java/ru/hse/goodtrip/ui/feed/FeedAdapter.java
@@ -1,49 +1,46 @@
 package ru.hse.goodtrip.ui.feed;
 
-import android.content.ClipData;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
 import java.util.Collections;
 import java.util.List;
-
-import ru.hse.goodtrip.databinding.ItemTripBinding;
+import ru.hse.goodtrip.databinding.PostTripBinding;
 import ru.hse.goodtrip.model.Trip;
 
 public class FeedAdapter extends RecyclerView.Adapter<FeedAdapter.FeedHolder> {
 
-    List<Trip> trips = Collections.emptyList();
+  List<Trip> trips = Collections.emptyList();
 
-    public void setTrips(List<Trip> newTrips) {
-        trips = newTrips;
-        notifyDataSetChanged();
-    }
+  public void setTrips(List<Trip> newTrips) {
+    trips = newTrips;
+    notifyDataSetChanged();
+  }
 
-    @NonNull
-    @Override
-    public FeedHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        ItemTripBinding binding = ItemTripBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
-        return new FeedHolder(binding);
-    }
+  @NonNull
+  @Override
+  public FeedHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    PostTripBinding binding = PostTripBinding.inflate(LayoutInflater.from(parent.getContext()),
+        parent, false);
+    return new FeedHolder(binding);
+  }
 
-    @Override
-    public void onBindViewHolder(@NonNull FeedHolder holder, int position) {
-        Trip trip = trips.get(position);
+  @Override
+  public void onBindViewHolder(@NonNull FeedHolder holder, int position) {
+    Trip trip = trips.get(position);
 //        var binding
-    }
+  }
 
-    @Override
-    public int getItemCount() {
-        return trips.size();
-    }
+  @Override
+  public int getItemCount() {
+    return trips.size();
+  }
 
-    public class FeedHolder extends RecyclerView.ViewHolder {
-        FeedHolder(ItemTripBinding binding) {
-            super(binding.getRoot());
-        }
+  public class FeedHolder extends RecyclerView.ViewHolder {
+
+    FeedHolder(PostTripBinding binding) {
+      super(binding.getRoot());
     }
+  }
 }
