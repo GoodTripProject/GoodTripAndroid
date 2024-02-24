904fbc4d86fed2867e40378e697195ba75315286
MaksimkaSH <shteinmaks@gmail.com>
2024-02-24 18:07:41 +0300

    add handle and full name fields

--- app/src/main/java/ru/hse/goodtrip/ui/profile/ProfileFragment.java
+++ app/src/main/java/ru/hse/goodtrip/ui/profile/ProfileFragment.java
@@ -1,18 +1,23 @@
 package ru.hse.goodtrip.ui.profile;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.lifecycle.ViewModelProvider;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
+
 import com.github.dhaval2404.imagepicker.ImagePicker;
+
 import ru.hse.goodtrip.databinding.FragmentProfileBinding;
 import ru.hse.goodtrip.ui.places.PlacesViewModel;
 
