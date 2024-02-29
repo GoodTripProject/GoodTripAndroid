d9495d442e012401f3537ade65be74e734fa4d93
cowboymalboro1884 <nejkovdaniil@gmail.com>
2024-02-29 14:16:59 +0300

    feat: add image loading library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
 }
 
 dependencies {
-
+    implementation("com.github.bumptech.glide:glide:4.16.0")
     implementation("com.google.android.libraries.identity.googleid:googleid:<latest version>")
     implementation("androidx.credentials:credentials:1.3.0-alpha01")
     implementation("androidx.credentials:credentials-play-services-auth:1.3.0-alpha01")
