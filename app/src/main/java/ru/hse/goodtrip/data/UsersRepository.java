64535905853a7e8b394b77a9c8952bf4554d4c10
Andrew Bolotsky <andrew04tlt@gmail.com>
2024-03-23 00:54:02 +0300

    feat: create network login of user

--- app/src/main/java/ru/hse/goodtrip/data/UsersRepository.java
+++ app/src/main/java/ru/hse/goodtrip/data/UsersRepository.java
@@ -1,7 +1,16 @@
 package ru.hse.goodtrip.data;
 
+import java.io.IOException;
+import lombok.Setter;
+import retrofit2.Call;
+import retrofit2.Response;
 import ru.hse.goodtrip.data.model.Result;
 import ru.hse.goodtrip.data.model.User;
+import ru.hse.goodtrip.network.NetworkManager;
+import ru.hse.goodtrip.network.authentication.LoginService;
+import ru.hse.goodtrip.network.authentication.model.AuthenticationResponse;
+import ru.hse.goodtrip.network.authentication.model.AuthorizationRequest;
+import ru.hse.goodtrip.network.authentication.model.RegisterRequest;
 
 /**
  * Class that requests authentication and user information from the remote data source and maintains
@@ -11,12 +20,14 @@ public class UsersRepository {
 
   private static volatile UsersRepository instance;
 
-  private final AuthService authService;
+  private final LoginService loginService;
 
   private User user = null;
+  @Setter
+  private String userToken;
 
   private UsersRepository() {
-    this.authService = new AuthService();
+    this.loginService = NetworkManager.getInstance().getInstanceOfService(LoginService.class);
   }
 
   /**
@@ -40,8 +51,7 @@ public class UsersRepository {
   }
 
   public void logout() {
-    user = null;
-    authService.logout();
+    //TODO
   }
 
   /**
@@ -61,11 +71,25 @@ public class UsersRepository {
    * @return result value.
    */
   public Result login(String username, String password) {
-    Result result = authService.login(username, password);
-    if (result.isSuccess()) {
-      setLoggedInUser(((Result.Success) result).getData());
+    Call<AuthenticationResponse> loginServiceCall = loginService.login(
+        new AuthorizationRequest(username, password));
+    try {
+      loginServiceCall.wait();
+      Response<AuthenticationResponse> response = loginServiceCall.execute();
+      if (!response.isSuccessful()) {
+        return new Result.Error(new InterruptedException("Call of login failed"));
+      }
+      AuthenticationResponse authenticationResponse = response.body();
+      if (authenticationResponse == null) {
+        return new Result.Error(new InterruptedException("Call of login failed"));
+      }
+      setLoggedInUser(
+          new User(authenticationResponse.getName() + " " + authenticationResponse.getSurname(),
+              authenticationResponse.getUrl()));
+    } catch (InterruptedException | IOException e) {
+      return new Result.Error(e);
     }
-    return result;
+    return new Result.Success(user);
   }
 
   /**
@@ -80,10 +104,25 @@ public class UsersRepository {
    */
   public Result signUp(String username, String password, String handle, String name,
       String surname) {
-    Result result = authService.signUp(username, password, handle, name, surname);
-    if (result.isSuccess()) {
-      setLoggedInUser(((Result.Success) result).getData());
+    Call<AuthenticationResponse> registerServiceCall = loginService.register(
+        new RegisterRequest(username, handle, password, name, surname));
+    try {
+      registerServiceCall.wait();
+      Response<AuthenticationResponse> response = registerServiceCall.execute();
+      if (!response.isSuccessful()) {
+        return new Result.Error(new InterruptedException("Call of login failed"));
+      }
+      AuthenticationResponse authenticationResponse = response.body();
+      if (authenticationResponse == null) {
+        return new Result.Error(new InterruptedException("Call of login failed"));
+      }
+      userToken = authenticationResponse.getToken();
+      setLoggedInUser(
+          new User(authenticationResponse.getName() + " " + authenticationResponse.getSurname(),
+              authenticationResponse.getUrl()));
+    } catch (InterruptedException | IOException e) {
+      return new Result.Error(e);
     }
-    return result;
+    return new Result.Success(user);
   }
 }
