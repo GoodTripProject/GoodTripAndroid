3d6c9cf82cc493a6f2d267a5a86389332940236b
MaksimkaSH <shteinmaks@gmail.com>
2024-03-26 00:12:19 +0300

    feat: implement getUserTrips request with callbacks

--- app/src/main/java/ru/hse/goodtrip/data/TripRepository.java
+++ app/src/main/java/ru/hse/goodtrip/data/TripRepository.java
@@ -1,10 +1,15 @@
 package ru.hse.goodtrip.data;
 
+import androidx.annotation.NonNull;
+
 import java.io.IOException;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.Callback;
 import retrofit2.Response;
+import ru.hse.goodtrip.data.model.Result;
+import ru.hse.goodtrip.data.model.ResultHolder;
 import ru.hse.goodtrip.data.model.tripRequests.model.AddCountryRequest;
 import ru.hse.goodtrip.data.model.tripRequests.model.AddNoteRequest;
 import ru.hse.goodtrip.data.model.tripRequests.model.AddTripRequest;
@@ -23,6 +28,8 @@ public class TripRepository {
     }
 
     private String authToken;
+
+    private List<Trip> trips;
 
     public void setAuthToken(String authToken) {
         this.authToken = "Bearer " + authToken;
@@ -48,10 +55,44 @@ public class TripRepository {
         }
     }
 
-    public List<Trip> getUserTrips(Integer userId) {
-        Call<List<Trip>> getTripsCall = tripService.getUserTrips(userId, authToken);
-        return sendRequest(getTripsCall);
+    private <T> Callback<T> getCallback(ResultHolder<T> resultHolder, String errorMessage) {
+        return new Callback<T>() {
+            /** @noinspection unchecked*/
+            @Override
+            public void onResponse(@NonNull Call<T> call, @NonNull Response<T> response) {
+                synchronized (resultHolder) {
+                    T responseBody = response.body();
+                    if (responseBody == null) {
+                        resultHolder.setResult(
+                                new Result.Error<>(new InterruptedException(errorMessage))
+                        );
+                    } else {
+                        if (responseBody instanceof List) {
+                            trips = (List<Trip>) responseBody;//TODO глянуть нет ли ошибки
+                        }
+                        resultHolder.setResult(new Result.Success<>(responseBody));
+                    }
+                    resultHolder.notify();
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<T> call, @NonNull Throwable throwable) {
+                synchronized (resultHolder) {
+                    resultHolder.setResult(new Result.Error<>(new InterruptedException(errorMessage)));
+                    resultHolder.notify();
+                }
+            }
+        };
     }
+
+    public ResultHolder<List<Trip>> getUserTrips(Integer userId) {
+        ResultHolder<List<Trip>> resultHolder = new ResultHolder<>();
+        Call<List<Trip>> getTripsCall = tripService.getUserTrips(userId, authToken);
+        getTripsCall.enqueue(getCallback(resultHolder, "No trips"));
+        return resultHolder;
+    }
+
 
     public Trip getTripById(Integer tripId) {
         Call<Object> getTripCall = tripService.getTripById(tripId, authToken);
