2d4e699cf1cf69ae4ff019455846b1fbeef94664
Болоцкий Андрей Сергеевич <asbolotskiy@edu.hse.ru>
2024-05-14 20:25:50 +0300

    feat: make PlacesRepository singleton

--- app/src/main/java/ru/hse/goodtrip/data/PlacesRepository.java
+++ app/src/main/java/ru/hse/goodtrip/data/PlacesRepository.java
@@ -2,8 +2,6 @@ package ru.hse.goodtrip.data;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import org.locationtech.jts.geom.CoordinateXY;
-import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.geom.Point;
 import retrofit2.Call;
 import ru.hse.goodtrip.data.model.Result;
@@ -15,17 +13,15 @@ import ru.hse.goodtrip.network.places.model.PlaceResponse;
 
 public class PlacesRepository extends AbstractRepository {
 
+  private static volatile PlacesRepository instance;
+
   private final PlacesService placesService;
-  private static final GeometryFactory factory = new GeometryFactory();
 
   private PlacesRepository() {
     super();
     placesService = NetworkManager.getInstance().getInstanceOfService(PlacesService.class);
   }
 
-  private static Point createNewPoint(double x, double y) {
-    return factory.createPoint(new CoordinateXY(x, y));
-  }
 
   @SuppressWarnings("unchecked")
   public CompletableFuture<Result<Point>> getPlaceCoordinate(String placeName, String token) {
@@ -39,7 +35,30 @@ public class PlacesRepository extends AbstractRepository {
         PlaceResponse response = ((List<PlaceResponse>) result).get(0);
         return new Result.Success<>(createNewPoint(response.getLat(), response.getLng()));
       }
-      return new Result.Error<>(new Exception("Invalid Request"));
+      return new Result.Error<>(new Exception(result.toString()));
     });
+  }
+
+  @SuppressWarnings("unchecked")
+  /*public CompletableFuture<Result<List<PlaceResponse>>> getNearPlaces(String placeName, int radius, @Nullable String rankBy, @Nullable PlacesTypes type, String token) {
+    ResultHolder<Object> resultHolder = new ResultHolder<>();
+    Call<Object> getTripCall = placesService.getNearPlaces(
+        new PlaceRequest(placeName, radius, rankBy, type), getWrappedToken(token));
+    getTripCall.enqueue(getCallback(resultHolder, "Note with this id not exists", (result) -> {
+    }));
+    return getCompletableFuture(resultHolder).thenApplyAsync(result -> {
+      if (result instanceof List) {
+        List<PlaceResponse> response = ((List<PlaceResponse>) result);
+        return new Result.Success<>(response);
+      }
+      return new Result.Error<>(new Exception(result.toString()));
+    });
+  }*/
+
+  static public PlacesRepository getInstance() {
+    if (instance == null) {
+      instance = new PlacesRepository();
+    }
+    return instance;
   }
 }
