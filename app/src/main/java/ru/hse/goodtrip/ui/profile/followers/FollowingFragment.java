653e7ff078da374029a9c6d8b26ab814dc9e5d24
Болоцкий Андрей Сергеевич <asbolotskiy@edu.hse.ru>
2024-06-02 19:58:02 +0300

    feat: merge new ui and network processing

--- app/src/main/java/ru/hse/goodtrip/ui/profile/followers/FollowingFragment.java
+++ app/src/main/java/ru/hse/goodtrip/ui/profile/followers/FollowingFragment.java
@@ -1,9 +1,11 @@
-package ru.hse.goodtrip.ui.profile.followers;
+package ru.hse.goodtrip.ui.profile.following;
 
 import static ru.hse.goodtrip.ui.profile.followers.FollowingFragment.PAGE_TYPE.FOLLOWING;
 import static ru.hse.goodtrip.ui.trips.feed.utils.Utils.setImageByUrl;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +16,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import ru.hse.goodtrip.MainActivity;
 import ru.hse.goodtrip.data.UsersRepository;
 import ru.hse.goodtrip.data.model.User;
@@ -33,9 +36,9 @@ public class FollowingFragment extends Fragment {
   @Override
   public void onResume() {
     super.onResume();
-    ((MainActivity) requireActivity()).getSupportActionBar().show();
-    ((MainActivity) requireActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    ((MainActivity) requireActivity()).getSupportActionBar().setDisplayShowHomeEnabled(true);
+    Objects.requireNonNull(((MainActivity) requireActivity()).getSupportActionBar()).show();
+    Objects.requireNonNull(((MainActivity) requireActivity()).getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+    Objects.requireNonNull(((MainActivity) requireActivity()).getSupportActionBar()).setDisplayShowHomeEnabled(true);
   }
 
   @Override
@@ -51,6 +54,7 @@ public class FollowingFragment extends Fragment {
   @Override
   public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
+    updateFollowing();
     Bundle args = getArguments();
     if (args != null) {
       this.pageType = (PAGE_TYPE) args.get(ProfileFollowingFragment.PAGE_TYPE_ARG);
@@ -59,7 +63,7 @@ public class FollowingFragment extends Fragment {
       followingViewModel.setUser(user);
     }
 
-    renderFollowing();
+    updateFollowing();
     setButtonClickListeners();
   }
 
@@ -74,12 +78,15 @@ public class FollowingFragment extends Fragment {
     }
   }
 
+  private void updateFollowing() {
+    followingViewModel.updateUsers(
+        () -> new Handler(Looper.getMainLooper()).post(this::renderFollowing));
+  }
+
   private void renderFollowing() {
     LinearLayout users = binding.following;
 
-    List<User> usersToShow = followingViewModel.getUsers();
-
-    for (User follow : usersToShow) {
+    for (User follow : followingViewModel.getUsers()) {
       ItemFollowingBinding followingBinding = ItemFollowingBinding.inflate(
           getLayoutInflater());
       followingBinding.displayName.setText(follow.getDisplayName());
--- app/src/main/java/ru/hse/goodtrip/ui/profile/followers/FollowingViewModel.java
+++ app/src/main/java/ru/hse/goodtrip/ui/profile/followers/FollowingViewModel.java
@@ -1,10 +1,9 @@
-package ru.hse.goodtrip.ui.profile.followers;
+package ru.hse.goodtrip.ui.profile.following;
 
 import androidx.lifecycle.ViewModel;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.Getter;
-import lombok.Setter;
 import ru.hse.goodtrip.data.UsersRepository;
 import ru.hse.goodtrip.data.model.User;
 
@@ -13,13 +12,31 @@ import ru.hse.goodtrip.data.model.User;
  */
 public class FollowingViewModel extends ViewModel {
 
+  CommunicationRepository communicationRepository = CommunicationRepository.getInstance();
   @Getter
   @Setter
   private List<User> users = new ArrayList<>(); // TODO: get following
   @Setter
   private User user;
 
-  FollowingViewModel() {
-    users.add(UsersRepository.getInstance().getLoggedUser());
+  public void updateUsers(Runnable uiUpdate) {
+    communicationRepository.getFollowers(UsersRepository.getInstance().user.getId(),
+            UsersRepository.getInstance().user.getToken())
+        .thenAcceptAsync(
+            (newUsers) -> {
+              if (newUsers.isSuccess()) {
+                users = ((Result.Success<List<ru.hse.goodtrip.network.social.entities.User>>) newUsers).getData()
+                    .stream().map(networkUser -> {
+                      try {
+                        return new User(networkUser.getId(), networkUser.getHandle(),
+                            networkUser.getName() + " " + networkUser.getSurname(),
+                            new URL(networkUser.getImageLink()), "");
+                      } catch (MalformedURLException ignored) {
+                      }
+                      return null;
+                    }).collect(Collectors.toList());
+              }
+            }
+        ).thenRunAsync(uiUpdate);
   }
 }
