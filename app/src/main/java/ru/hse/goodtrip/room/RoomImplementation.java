b2b71c9a8cd0c10ec1265fed1b7d6229654b3423
cowboymalboro1884 <nejkovdaniil@gmail.com>
2024-06-09 01:09:39 +0300

    refactor: decompose fields for testing

--- app/src/main/java/ru/hse/goodtrip/room/RoomImplementation.java
+++ app/src/main/java/ru/hse/goodtrip/room/RoomImplementation.java
@@ -1,6 +1,7 @@
 package ru.hse.goodtrip.room;
 
 import android.app.Application;
+import android.content.Context;
 import androidx.room.Room;
 import java.util.List;
 import ru.hse.goodtrip.room.entities.UserEntity;
@@ -15,12 +16,14 @@ public class RoomImplementation extends Application {
   private static RoomImplementation instance;
   private LocalStorage localStorage;
 
+  private Context context;
+
   public static RoomImplementation getInstance() {
     return instance;
   }
 
   public UserEntity getLoggedUser() {
-    List<UserEntity> users = localStorage.userDao().getUser();
+    List<UserEntity> users = getLocalStorage().userDao().getUser();
     if (!users.isEmpty()) {
       return users.get(0);
     }
@@ -29,25 +32,23 @@ public class RoomImplementation extends Application {
 
   public void setLoggedUser(String name, String password) {
     if (getLoggedUser() == null) {
-      localStorage.userDao().insert(
+      getLocalStorage().userDao().insert(
           new UserEntity(USER_KEY, name, password));
     } else {
-      localStorage.userDao().update(
+      getLocalStorage().userDao().update(
           new UserEntity(USER_KEY, name, password));
     }
-    AppPreferences.setUserLoggedIn(this, true);
   }
 
   public void logOutUser() {
     UserEntity user = getLoggedUser();
     if (user != null) {
-      localStorage.userDao().deleteById(user.uid);
+      getLocalStorage().userDao().deleteById(user.uid);
     }
-    AppPreferences.setUserLoggedIn(this, false);
   }
 
   public boolean isUserLoggedIn() {
-    return AppPreferences.isUserLoggedIn(this);
+    return getLoggedUser() != null;
   }
 
   @Override
@@ -55,6 +56,7 @@ public class RoomImplementation extends Application {
     super.onCreate();
 
     instance = this;
+    context = this;
     localStorage = Room.databaseBuilder(getApplicationContext(), LocalStorage.class, DATABASE_NAME)
         .allowMainThreadQueries()
         .build();
@@ -62,5 +64,9 @@ public class RoomImplementation extends Application {
 
   public LocalStorage getLocalStorage() {
     return localStorage;
+  }
+
+  public Context getContext() {
+    return context;
   }
 }
