5fe642dbf83ceca10530a2c7f0ad0d1c7d9fe540
cowboymalboro1884 <nejkovdaniil@gmail.com>
2024-06-09 01:09:14 +0300

    test: add Room tests

--- app/src/androidTest/java/ru/hse/goodtrip/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/ru/hse/goodtrip/ExampleInstrumentedTest.java
@@ -8,11 +8,6 @@ import androidx.test.platform.app.InstrumentationRegistry;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
 
