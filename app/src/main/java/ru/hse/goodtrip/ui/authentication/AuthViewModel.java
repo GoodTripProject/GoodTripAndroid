52c4133a57dee239a5b9b8331e13e41ea5e02124
Болоцкий Андрей Сергеевич <asbolotskiy@edu.hse.ru>
2024-06-09 21:11:08 +0300

    refactor: comment password hashing

--- app/src/main/java/ru/hse/goodtrip/ui/authentication/AuthViewModel.java
+++ app/src/main/java/ru/hse/goodtrip/ui/authentication/AuthViewModel.java
@@ -48,10 +48,10 @@ public class AuthViewModel extends ViewModel {
    * @param password password
    */
   public void login(String username, String password) {
-    String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
+    // String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
 
     CompletableFuture<Result<AuthenticationResponse>> result = usersRepository.login(username,
-        hashedPassword);
+        password);
     runExecutorToWaitResult(result,
         () -> loginResult.setValue(new LoginResult(R.string.login_failed)));
   }
@@ -86,10 +86,10 @@ public class AuthViewModel extends ViewModel {
   public void signUp(String username, String password, String name, String surname,
       String handle) {
     loginDataChanged(username, password);
-    String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
+    // String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
 
     CompletableFuture<Result<AuthenticationResponse>> result = usersRepository.signUp(username,
-        hashedPassword, handle,
+        password, handle,
         surname, name);
     runExecutorToWaitResult(result,
         () -> loginResult.setValue(new LoginResult(R.string.login_failed)));
